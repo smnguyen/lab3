863756398b37150a68c9c8bb31bbb35680208d02
Sammy Nguyen <smnguyen@stanford.edu>
2014-01-22 15:16:24 -0800

    Javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,39 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+        $('#testjs').text("Button clicked!");
+        $(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+
+    $('#submitBtn').click(function(e) {
+        var selectedProject = $($('#project').val());
+        selectedProject.animate({
+            width: $('#width').val()
+        });
+
+        var description = $(selectedProject).find('.project-description');
+        if (description.length > 0 && description.css("display") !== "none") {
+            description.text($('#description').val());
+        }
+    });
+}
+
+function projectClick(e) {
+    // Cancel the default action, which prevents the page from reloading
+    e.preventDefault();
+
+    // In an event listener, $(this) is the leement that fired the event
+    var containingProject = $(this).closest(".project"); 
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) { 
+        $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); 
+    } else if (description.css("display") === "none"){ 
+        description.show();
+    } else {
+        description.hide();
+    }
 }
